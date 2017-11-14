cbd9699871ae34447aad8ef84d2dae88e64c2c2c
Carra Spring Wu <carrawu@college.harvard.edu>
2017-11-14 14:02:33 +0000

    mesg

---
+++ application.py
@@ -0,0 +1,115 @@
+import os
+import re
+from flask import Flask, jsonify, render_template, request
+
+from cs50 import SQL
+from helpers import apology
+
+# Configure application
+app = Flask(__name__)
+
+# Configure CS50 Library to use SQLite database
+db2 = SQL("sqlite:///capone.db")
+
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route("/")
+def index():
+    """Render map"""
+    if not os.environ.get("API_KEY"):
+        raise RuntimeError("API_KEY not set")
+    return render_template("index.html", key=os.environ.get("API_KEY"))
+
+
+
+@app.route("/search", methods=['GET', 'POST'])
+def search():
+    """Search for places that match query"""
+
+    if request.method == "POST":
+
+        # Ensure latitude was submitted
+        if not request.form.get("lat"):
+            return apology("please provide latitude and longitude", 403)
+
+        # Ensure longitude was submitted
+        elif not request.form.get("long"):
+            return apology("please provide longitude and longitude", 403)
+
+        # rounds the entered coordinates to two decimal places, thus finding all the existing airbnb listings within a short distance (in the same neighborhood)
+        lat= "{0:.2f}".format(float(request.form.get("lat"))) + "%"
+        longi="{0:.2f}".format(float(request.form.get("long"))) + "%"
+
+
+        rows = db2.execute("SELECT price, reviews_per_month, latitude, longitude, neighbourhood_cleansed FROM listings WHERE latitude LIKE :lat AND longitude LIKE :longi", lat=lat, longi=longi)
+
+        # Redirect user to home page
+        return jsonify(rows)
+
+
+
+    # return jsonify(db.execute("SELECT * FROM listings WHERE latitude LIKE :q AND LONGITUDE LIKE :q2", q=q, q2=))
+@app.route("/bestneighborhood")
+def bestneighborhood():
+    rows = db2.execute("SELECT AVG(review_scores_rating), neighbourhood_cleansed FROM listings WHERE review_scores_rating NOT null AND review_scores_rating != '' GROUP BY neighbourhood_cleansed")
+    return jsonify(rows)
+
+@app.route("/otherdatapoints")
+def otherdatapoints():
+    rows = db2.execute("SELECT review_scores_rating, reviews_per_month FROM listings WHERE neighbourhood_cleansed LIKE 'Castro/Upper Market' AND review_scores_rating NOT null AND reviews_per_month !='' AND reviews_per_month != 223")
+    return jsonify(rows)
+
+@app.route("/datapoints")
+def datapoints():
+    rows = db2.execute("SELECT price, reviews_per_month FROM listings WHERE neighbourhood_cleansed LIKE 'Castro/Upper Market' AND price NOT null AND reviews_per_month !='' AND reviews_per_month != 223")
+    return jsonify(rows)
+
+@app.route("/update")
+def update():
+    """Find up to 10 places within view"""
+
+    # Ensure parameters are present
+    if not request.args.get("sw"):
+        raise RuntimeError("missing sw")
+    if not request.args.get("ne"):
+        raise RuntimeError("missing ne")
+
+    # Ensure parameters are in lat,lng format
+    if not re.search("^-?\d+(?:\.\d+)?,-?\d+(?:\.\d+)?$", request.args.get("sw")):
+        raise RuntimeError("invalid sw")
+    if not re.search("^-?\d+(?:\.\d+)?,-?\d+(?:\.\d+)?$", request.args.get("ne")):
+        raise RuntimeError("invalid ne")
+
+    # Explode southwest corner into two variables
+    sw_lat, sw_lng = map(float, request.args.get("sw").split(","))
+
+    # Explode northeast corner into two variables
+    ne_lat, ne_lng = map(float, request.args.get("ne").split(","))
+
+    # Find 10 cities within view, pseudorandomly chosen if more within view
+    if sw_lng <= ne_lng:
+
+        # Doesn't cross the antimeridian
+        rows = db2.execute("""SELECT * FROM listings
+                          WHERE :sw_lat <= latitude AND latitude <= :ne_lat AND (:sw_lng <= longitude AND longitude <= :ne_lng)
+                          GROUP BY country_code, city, state""",
+                          sw_lat=sw_lat, ne_lat=ne_lat, sw_lng=sw_lng, ne_lng=ne_lng)
+
+    else:
+
+        # Crosses the antimeridian
+        rows = db2.execute("""SELECT * FROM listings
+                          WHERE :sw_lat <= latitude AND latitude <= :ne_lat AND (:sw_lng <= longitude OR longitude <= :ne_lng)
+                          GROUP BY country_code, city, state""",
+                          sw_lat=sw_lat, ne_lat=ne_lat, sw_lng=sw_lng, ne_lng=ne_lng)
+
+    # Output places as JSON
+    return jsonify(rows)
---
+++ static/scripts.js
@@ -0,0 +1,411 @@
+
+// Google Map
+let map;
+
+// Markers for map
+let markers = [];
+
+// Info window
+let info = new google.maps.InfoWindow();
+
+
+// Execute when the DOM is fully loaded
+$(document).ready(function() {
+
+    // Styles for map
+    // https://developers.google.com/maps/documentation/javascript/styling
+    let styles = [
+
+        // Hide Google's labels
+        {
+            featureType: "all",
+            elementType: "labels",
+            stylers: [
+                {visibility: "off"}
+            ]
+        },
+
+        {
+            featureType: "administrative.neighborhood",
+            elementType: "labels",
+            stylers: [
+                {visibility: "on"}
+            ]
+        },
+
+        // Hide roads
+        {
+            featureType: "road",
+            elementType: "geometry",
+            stylers: [
+                {visibility: "off"}
+            ]
+        }
+
+    ];
+
+    // Options for map
+    // https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+    let options = {
+        center: {lat: 37.7749, lng: -122.399}, // Cambridge, MA
+        disableDefaultUI: true,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        maxZoom: 18,
+        minZoom: 12,
+        panControl: true,
+        styles: styles,
+        zoom: 13,
+        zoomControl: true
+    };
+
+    // Get DOM node in which map will be instantiated
+    let canvas = $("#map-canvas").get(0);
+
+    // Instantiate map
+    map = new google.maps.Map(canvas, options);
+
+    // Configure UI once Google Map is idle (i.e., loaded)
+    google.maps.event.addListenerOnce(map, "idle", configure);
+
+    google.charts.load('current', {'packages':['corechart']});
+
+
+    google.charts.setOnLoadCallback(drawChart);
+    google.charts.setOnLoadCallback(drawOtherChart);
+
+
+});
+
+function drawChart()
+{
+    var dataset = [];
+
+    var req = $.getJSON("/datapoints", function(data, textStatus, jqXHR) {
+
+        for (let i = 0; i < data.length; i++)
+        {
+           var obj = [data[i].price, data[i].reviews_per_month];
+           dataset.push(obj);
+
+        }
+
+    });
+
+
+
+    req.success(function(response){
+        dataset.sort(function(a, b)
+        {
+            var a_number = Number(a[0].replace(/[^0-9\.]+/g,""));
+            var b_number = Number(b[0].replace(/[^0-9\.]+/g,""));
+            return a_number - b_number;
+        });
+
+
+
+        console.log(dataset);
+
+        dataset.unshift(['price', 'bookings per month']);
+        var data1 = google.visualization.arrayToDataTable(dataset);
+
+        var options = {
+          title: 'Price vs Bookings',
+          curveType: 'function',
+          legend: { position: 'bottom' },
+          width: 475,
+          height: 300,
+          chartArea:{left:40, right: 20}
+        };
+
+        var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+        chart.draw(data1, options);
+    });
+
+}
+
+function drawOtherChart()
+{
+    var dataset = [];
+
+    var req = $.getJSON("/otherdatapoints", function(data, textStatus, jqXHR) {
+
+        for (let i = 0; i < data.length; i++)
+        {
+           var obj = [data[i].review_scores_rating, data[i].reviews_per_month];
+           dataset.push(obj);
+
+        }
+
+    });
+
+
+    req.success(function(response){
+        dataset.sort(function(a,b) {
+          return a[0] - b[0];
+        });
+        console.log(dataset);
+
+        dataset.unshift(['review score', 'bookings per month']);
+        var data1 = google.visualization.arrayToDataTable(dataset);
+
+        var options = {
+          title: 'Review Score vs Bookings',
+          curveType: 'function',
+          legend: { position: 'bottom' },
+          width: 475,
+          height: 300,
+          chartArea:{left:40, right: 20}
+        };
+
+        var chart = new google.visualization.ScatterChart(document.getElementById('curve_chart2'));
+
+
+        chart.draw(data1, options);
+    });
+
+}
+
+
+function addHouse(listing)
+{
+    var myLatlng = new google.maps.LatLng(listing["latitude"], listing["longitude"]);
+    var image = "https://www.waltersnissan.com/assets/d1772/img/btn_map_pointer.png";
+
+    //https://www.yarraranges.vic.gov.au/files/content/public/lists/do-it-online-listing/rates/rates-icon.png
+    // house icon for later
+
+    var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        label: listing["name"] + ", " + listing["price"],
+        title: listing["name"] + ", " + listing["price"],
+        icon: image
+    });
+
+
+    // To add the marker to the map, call setMap();
+    markers.push(marker);
+
+    google.maps.event.addListener(marker, 'click', function(event)
+    {
+        let list = "<ul>";
+        list += "Neighborhood: " + listing["neighbourhood_cleansed"] + ", ";
+        list += "Accommodates: " + listing["accommodates"];
+        list+="</ul>";
+        showInfo(marker, list);
+    });
+}
+
+
+function addProperty()
+{
+
+    var lat = document.getElementById("la").value;
+    var long = document.getElementById("lng").value;
+
+    var myLatlng = new google.maps.LatLng(lat, long);
+
+
+    var image = "https://www.yarraranges.vic.gov.au/files/content/public/lists/do-it-online-listing/rates/rates-icon.png";
+
+
+    var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        label: "YOUR PROPERTY",
+        title: "home",
+        icon: image
+    });
+
+
+    // To add the marker to the map, call setMap();
+    markers.push(marker);
+
+
+    var sum = 0;
+    var weeklyrevenue;
+    var maxweeklyrevenue = 0;
+    var maxindex;
+    var avgweeklyrevenue;
+
+    $.getJSON("/search", function(data, textStatus, jqXHR) {
+        console.log(data);
+        for (i = 0; i<data.length; i++)
+        {
+            weeklyrevenue = data[i].price * data[i].reviews_per_month/4;
+            if (weeklyrevenue > maxweeklyrevenue)
+            {
+                maxweeklyrevenue = weeklyrevenue;
+                maxindex =i;
+            }
+            sum += weeklyrevenue;
+        }
+        avgweeklyrevenue = sum / data.length;
+
+        let div = "<div id='info'><p>The average weekly revenue is " + avgweeklyrevenue + ". You can maximize revenue at a price per night of " + data[maxindex].price + ".</p></div>";
+
+        // Set info window's content
+        info.setContent(div);
+
+        // Open info window (if not already open)
+        info.open(map, marker);
+
+    });
+
+}
+
+$(function() {
+    $('form').submit(function() {
+        addProperty();
+
+        $.ajax({
+            type: 'POST',
+            url: '/search',
+            data: { lat: $(this).lat.value,
+                    lng: $(this).long.value }
+        });
+
+        return false;
+    });
+})
+
+
+
+
+// Configure application
+function configure()
+{
+    // Update UI after map has been dragged
+    google.maps.event.addListener(map, "dragend", function() {
+
+        // If info window isn't open
+        // http://stackoverflow.com/a/12410385
+        if (!info.getMap || !info.getMap())
+        {
+            update();
+        }
+
+        //stay in SF
+        var strictBounds = new google.maps.LatLngBounds(
+            new google.maps.LatLng(36.00, -124.00),
+            new google.maps.LatLng(40.00, -120.00)
+        );
+
+        if (strictBounds.contains(map.getCenter())) return;
+
+         // We're out of bounds - Move the map back within the bounds
+
+         var c = map.getCenter(),
+             x = c.lng(),
+             y = c.lat(),
+             maxX = strictBounds.getNorthEast().lng(),
+             maxY = strictBounds.getNorthEast().lat(),
+             minX = strictBounds.getSouthWest().lng(),
+             minY = strictBounds.getSouthWest().lat();
+
+         if (x < minX) x = minX;
+         if (x > maxX) x = maxX;
+         if (y < minY) y = minY;
+         if (y > maxY) y = maxY;
+
+         map.setCenter(new google.maps.LatLng(y, x));
+    });
+
+
+    // Update UI after zoom level changes
+    google.maps.event.addListener(map, "zoom_changed", function() {
+        update();
+        if (map.getZoom() < 13) map.setZoom(13);
+    });
+
+
+
+    // Hide info window when text box has focus
+    $("#q").focus(function(eventData) {
+        info.close();
+    });
+
+    // Re-enable ctrl- and right-clicking (and thus Inspect Element) on Google Map
+    // https://chrome.google.com/webstore/detail/allow-right-click/hompjdfbfmmmgflfjdlnkohcplmboaeo?hl=en
+    document.addEventListener("contextmenu", function(event) {
+        event.returnValue = true;
+        event.stopPropagation && event.stopPropagation();
+        event.cancelBubble && event.cancelBubble();
+    }, true);
+
+
+    // Update UI
+    update();
+
+    // Give focus to text box
+    $("#q").focus();
+}
+
+
+// Remove markers from map
+function removeMarkers()
+{
+    for(var i = 0; i < markers.length; i++)
+    {
+        markers[i].setMap(null);
+    }
+    markers = [];
+}
+
+
+
+
+// Show info window at marker with content
+function showInfo(marker, content)
+{
+    // Start div
+    let div = "<div id='info'>";
+    if (typeof(content) == "undefined")
+    {
+        // http://www.ajaxload.info/
+        div += "<img alt='loading' src='/static/ajax-loader.gif'/>";
+    }
+    else
+    {
+        div += content;
+    }
+
+    // End div
+    div += "</div>";
+
+    // Set info window's content
+    info.setContent(div);
+
+    // Open info window (if not already open)
+    info.open(map, marker);
+}
+
+
+// Update UI's markers
+function update()
+{
+    // Get map's bounds
+    let bounds = map.getBounds();
+    let ne = bounds.getNorthEast();
+    let sw = bounds.getSouthWest();
+
+    // Get places within bounds (asynchronously)
+    let parameters = {
+        ne: `${ne.lat()},${ne.lng()}`,
+        q: $("#q").val(),
+        sw: `${sw.lat()},${sw.lng()}`
+    };
+    $.getJSON("/update", parameters, function(data, textStatus, jqXHR) {
+
+       // Remove old markers from map
+       removeMarkers();
+
+       // Add new markers to map
+       for (let i = 0; i < data.length; i++)
+       {
+           addHouse(data[i]);
+       }
+
+    });
+};
